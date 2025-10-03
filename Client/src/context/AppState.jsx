b446ab7f7c4f9c620213bd3e3f91ead36b61b472
Saurabh Kumar <146050167+Saurabh-Kumar20-24@users.noreply.github.com>
2025-10-03 22:06:29 +0530

    adding backend url in frontend

--- Client/src/context/AppState.jsx
+++ Client/src/context/AppState.jsx
@@ -7,7 +7,7 @@ import "react-toastify/dist/ReactToastify.css";
 const AppState = (props) => {
   // const url = "http://localhost:1000/api";
 
-  const url = "http://localhost:1000/api";
+  const url = "https://mern-e-commerce-1-10at.onrender.com/api";
 
 
   const [products, setProducts] = useState([]);
